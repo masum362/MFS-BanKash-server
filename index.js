9ad093177cd59748d40837c95922b1a3809bdf98
masum362 <masumahmed64077@gmail.com>
2024-07-14 23:53:02 +0600

    successfully server listening

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+app.use(cors({
+    credentials:true,
+    origin:['http://localhost:5173']
+}))
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`server  listening on ${port}`);
+});
